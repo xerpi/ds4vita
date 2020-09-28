5f96ff3ed3e7bb4a6fc798b671af5a4c0e2ca7b8
Mer1e <merlev.inbox@gmail.com>
2020-09-28 17:42:06 +0300

    [+] Touch scaling for ds4 updated

--- main.c
+++ main.c
@@ -28,6 +28,8 @@ int __errno;
 
 #define DS4_TOUCHPAD_W 1920
 #define DS4_TOUCHPAD_H 940
+#define DS4_TOUCHPAD_W_DEAD 60
+#define DS4_TOUCHPAD_H_DEAD 120
 #define DS4_JOYSTICK_THRESHOLD 5
 #define DS4_TRIGGER_THRESHOLD 0
 
@@ -766,6 +768,10 @@ DECL_FUNC_HOOK_PATCH_CTRL(ksceCtrlReadBufferPositive,    SPACE_KERNEL, LOGIC_POS
 DECL_FUNC_HOOK_PATCH_CTRL(ksceCtrlPeekBufferNegative,    SPACE_KERNEL, LOGIC_NEGATIVE, TRIGGERS_NONEXT)
 DECL_FUNC_HOOK_PATCH_CTRL(ksceCtrlReadBufferNegative,    SPACE_KERNEL, LOGIC_NEGATIVE, TRIGGERS_NONEXT)
 
+static int rescaleTouchCoordinate(int ds4coord, int ds4size, int ds4dead, int vitasize){
+	return clamp(((ds4coord - ds4dead) * vitasize) / (ds4size - ds4dead * 2), 0, vitasize - 1);
+}
+
 static void patch_touch_data(SceUInt32 port, SceTouchData *pData, SceUInt32 nBufs,
 			    struct ds4_input_report *ds4)
 {
@@ -777,17 +783,27 @@ static void patch_touch_data(SceUInt32 port, SceTouchData *pData, SceUInt32 nBuf
 	for (i = 0; i < nBufs; i++) {
 		unsigned int num_reports = 0;
 
+		// If finger1 present, add finger1 as report 0
 		if (!ds4->finger1_activelow) {
-			pData->report[0].id = ds4->finger1_id;
-			pData->report[0].x = (ds4->finger1_x * VITA_FRONT_TOUCHSCREEN_W) / DS4_TOUCHPAD_W;
-			pData->report[0].y = (ds4->finger1_y * VITA_FRONT_TOUCHSCREEN_H) / DS4_TOUCHPAD_H;
+			pData[i].report[0].id = ds4->finger1_id;
+			pData[i].report[0].x = rescaleTouchCoordinate(ds4->finger1_x, DS4_TOUCHPAD_W, DS4_TOUCHPAD_W_DEAD, VITA_FRONT_TOUCHSCREEN_W);
+			pData[i].report[0].y = rescaleTouchCoordinate(ds4->finger1_y, DS4_TOUCHPAD_H, DS4_TOUCHPAD_H_DEAD, VITA_FRONT_TOUCHSCREEN_H);
 			num_reports++;
 		}
 
-		if (!ds4->finger2_activelow) {
-			pData->report[1].id = ds4->finger2_id;
-			pData->report[1].x = (ds4->finger2_x * VITA_FRONT_TOUCHSCREEN_W) / DS4_TOUCHPAD_W;
-			pData->report[1].y = (ds4->finger2_y * VITA_FRONT_TOUCHSCREEN_H) / DS4_TOUCHPAD_H;
+		// If only finger2 is present, add finger2 as report 0
+		if (!ds4->finger2_activelow && ds4->finger1_activelow) {
+			pData[i].report[0].id = ds4->finger2_id;
+			pData[i].report[0].x = rescaleTouchCoordinate(ds4->finger2_x, DS4_TOUCHPAD_W, DS4_TOUCHPAD_W_DEAD, VITA_FRONT_TOUCHSCREEN_W);
+			pData[i].report[0].y = rescaleTouchCoordinate(ds4->finger2_y, DS4_TOUCHPAD_H, DS4_TOUCHPAD_H_DEAD, VITA_FRONT_TOUCHSCREEN_H);
+			num_reports++;
+		}
+
+		// If both finger1 and finger2 present, add finger2 as report 1
+		if (!ds4->finger2_activelow && !ds4->finger1_activelow) {
+			pData[i].report[1].id = ds4->finger2_id;
+			pData[i].report[1].x = rescaleTouchCoordinate(ds4->finger2_x, DS4_TOUCHPAD_W, DS4_TOUCHPAD_W_DEAD, VITA_FRONT_TOUCHSCREEN_W);
+			pData[i].report[1].y = rescaleTouchCoordinate(ds4->finger2_y, DS4_TOUCHPAD_H, DS4_TOUCHPAD_H_DEAD, VITA_FRONT_TOUCHSCREEN_H);
 			num_reports++;
 		}
 
